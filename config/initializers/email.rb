f2a421c76b6b46a26e9348d77d20bd338c6aad72
Jae Young Ryoo <jay.ryoo@gmail.com>
2014-02-19 13:20:57 -0800

    testing email

--- config/initializers/email.rb
+++ config/initializers/email.rb
@@ -1,5 +1,5 @@
 ActionMailer::Base.smtp_settings = {
-  :address => "127.0.0.1",
+  :address => "smtp.gmail.com",
   :port => 25,
   :domain => Rails.application.domain,
   :enable_starttls_auto => false,

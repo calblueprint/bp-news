943e390545d6dd25fd4b33a1c6e2a0ef1ffdd403
Jae Young Ryoo <jay.ryoo@gmail.com>
2014-02-19 13:27:21 -0800

    testing email

--- app/mailers/invitation_mailer.rb
+++ app/mailers/invitation_mailer.rb
@@ -1,7 +1,6 @@
 class InvitationMailer < ActionMailer::Base
   default :from => "#{Rails.application.name} " <<
-    # "<nobody@#{Rails.application.domain}>"
-    "<nobody@calblueprint.org>"
+    "<nobody@#{Rails.application.domain}>
 
   def invitation(invitation)
     @invitation = invitation

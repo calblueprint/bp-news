79a2035343681fc1930a01a363efca4ba2010259
Mark Miyashita <negativetwelve@gmail.com>
2014-02-20 01:16:38 -0800

    Use calblueprint email address

--- config/initializers/email.rb
+++ config/initializers/email.rb
@@ -1,9 +1,9 @@
 ActionMailer::Base.smtp_settings = {
   :address => "smtp.gmail.com",
   :port => 587,
-  :user_name => "jay.ryoo@gmail.com",
-  :password => ENV['EMAIL_PASSWORD'],
+  :user_name => ENV["EMAIL_ADDRESS"],
+  :password => ENV["EMAIL_PASSWORD"],
   :authentication => "plain",
-  # :domain => Rails.application.domain,
+  :domain => Rails.application.domain,
   :enable_starttls_auto => true
 }

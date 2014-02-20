612c4361471d08ea95d92f38e04a4c2fa75c2a0d
Jae Young Ryoo <jay.ryoo@gmail.com>
2014-02-20 01:18:46 -0800

    remove debug puts

--- app/mailers/invitation_mailer.rb
+++ app/mailers/invitation_mailer.rb
@@ -5,12 +5,10 @@ class InvitationMailer < ActionMailer::Base
   def invitation(invitation)
     @invitation = invitation
 
-    puts "[USER_DEBUG] attempting to email " + invitation.email + " ..."
     mail(
       :to => invitation.email,
       subject: "[#{Rails.application.name}] You have been invited to " <<
         Rails.application.name
     )
-    puts "[USER_DEBUG] invitation to " + invitation.email + " mailed successfully"
   end
 end

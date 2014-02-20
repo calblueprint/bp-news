bc62d43a6ffb51ef1d177353d30889f69f473426
Mark Miyashita <negativetwelve@gmail.com>
2014-02-19 17:27:33 -0800

    Add search by user/story/comment

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,7 +5,18 @@ class SearchController < ApplicationController
     @cur_url = "/search"
 
     if params[:q].to_s.present?
-      @results = PgSearch.multisearch(params[:q])
+      case params[:what]
+      when "all"
+
+      when "users"
+        @results = User.search(params[:q])
+      when "stories"
+
+      when "comments"
+
+      else
+        @results = []
+      end
 
       if params[:page].present?
         @results = @results.paginate(params[:page].to_i)

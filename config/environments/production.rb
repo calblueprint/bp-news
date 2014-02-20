367065e0815286dda1af1ffddb64e5b5cb52aab1
Jae Young Ryoo <jay.ryoo@gmail.com>
2014-02-20 00:51:32 -0800

    testing email more

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,7 @@ Lobsters::Application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to

2b82ce5da294cab3d596c1a7f2fc4b637ac254ff
Mark Miyashita <negativetwelve@gmail.com>
2014-02-19 18:21:00 -0800

    Search for all does not include users

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,19 +3,21 @@ class SearchController < ApplicationController
   def index
     @title = "Search"
     @cur_url = "/search"
+    if params[:what].to_s.empty?
+      params[:what] = "all"
+    end
 
+    @results = []
     if params[:q].to_s.present?
       case params[:what]
       when "all"
-
+        @results = Story.search(params[:q]) + Comment.search(params[:q])
       when "users"
         @results = User.search(params[:q])
       when "stories"
-
+        @results = Story.search(params[:q])
       when "comments"
-
-      else
-        @results = []
+        @results = Comment.search(params[:q])
       end
 
       if params[:page].present?
